141e0ed2006049674ef7eaa9f13de7c72f239c34
SeyiDan <64979820+SeyiDan@users.noreply.github.com>
2025-04-30 22:41:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (90.34%)

--- difficulty/topic/0013-roman-to-integer/solution.py
+++ difficulty/topic/0013-roman-to-integer/solution.py
@@ -1,32 +1,13 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        d = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        roman_map = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
 
-        summ = 0
-        n = len(s)
-        i = 0
+        total = 0
 
-        while i < n:
-            if i < n-1 and d[s[i]] < d[s[i+1]]:
-                summ += d[s[i+1]] - d[s[i]]
-                i+=2
+        for i in range(len(s)-1):
+            if roman_map[s[i]] < roman_map[s[i+1]]:
+                total-=roman_map[s[i]]
             else:
-                summ+= d[s[i]]
-                i+=1
-        return summ
-
-
-
-
-        # summ = 0
-        # n = len(s)
-        # i = 0
-
-        # while i < n:
-        #     if i < n - 1 and d[s[i]] < d[s[i+1]]:
-        #         summ = d[s[i+1]] - d[s[i]]
-        #         i+=2
-        #     else:
-        #         summ += d[s[i]]
-        #         i+=1
-        # return summ
+                total+=roman_map[s[i]]
+        total+=roman_map[s[-1]]
+        return total
