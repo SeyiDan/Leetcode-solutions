fd87322a83bbb9ee29483774d817366885216e37
SeyiDan <oladejo.seyi2@gmail.com>
2025-10-16 04:11:51 +0000

    Code challenge submission for question: 217 Contains Duplicate

---
+++ 217 Contains Duplicate/217contains-duplicate.py
@@ -0,0 +1,18 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+
+
+
+
+
+
+
+        numss=set()
+
+        for i in nums:
+            if i in numss:
+                return True
+            numss.add(i)
+
+        return False
