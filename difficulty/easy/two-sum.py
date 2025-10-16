d40687eafcf511978fe270758902f3b5d310534c
leetcode-sync <actions@github.com>
2025-10-16 05:45:53 +0000

    Sync submission: Two Sum (2025-03-07)

--- difficulty/easy/two-sum.py
+++ difficulty/easy/two-sum.py
@@ -2,17 +2,20 @@
 # Difficulty: Easy
 # Language: Python3
 # Runtime: 0 ms
-# Submission Date: 2025-05-09
+# Submission Date: 2025-03-07
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        summ = {}
+    
+        store = {} #Dictionary to store the numbers
 
+        #find the two numbers
         for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in summ:
-                return [summ[diff], i]
-            summ[nums[i]]=i
-        return summ
-            
-           
+            complement = target - nums[i]
+            if complement in store:
+                return[store[complement],i]
+            store[nums[i]]=i
+        return store
+                
+
+
