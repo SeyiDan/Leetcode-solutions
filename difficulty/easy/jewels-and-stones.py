0ea3b180abbdb70a7174b74d0a2789467859d720
SeyiDan <64979820+SeyiDan@users.noreply.github.com>
2025-10-16 05:51:15 +0000

    Sync submission: Jewels and Stones (2025-10-16)

--- difficulty/easy/jewels-and-stones.py
+++ difficulty/easy/jewels-and-stones.py
@@ -1,16 +1,57 @@
 # Jewels and Stones
 # Difficulty: Easy
 # Language: Python3
-# Runtime: 0 ms
+# Runtime: 3 ms
 # Submission Date: 2025-10-16
 
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        jewel_set= set(jewels)
-        count=0
+        jewel_set = set(jewels)
+        count = 0
 
-        for stone in stones:
-            if stone in jewel_set:
+        for i in stones:
+            if i in jewel_set:
                 count+=1
         return count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # jewel_set= set(jewels)
+        # count=0
+
+        # for stone in stones:
+        #     if stone in jewel_set:
+        #         count+=1
+        # return count
         
