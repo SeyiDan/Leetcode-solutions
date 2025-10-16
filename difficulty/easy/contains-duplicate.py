8822c3652cbd84880583e2808411e2a963dd342a
leetcode-sync <actions@github.com>
2025-10-16 05:45:20 +0000

    Sync submission: Contains Duplicate (2025-10-16)

--- difficulty/easy/contains-duplicate.py
+++ difficulty/easy/contains-duplicate.py
@@ -1,17 +1,24 @@
 # Contains Duplicate
 # Difficulty: Easy
 # Language: Python3
-# Runtime: 11 ms
-# Submission Date: 2025-05-06
+# Runtime: 13 ms
+# Submission Date: 2025-10-16
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-
-        for num in nums:
-            if num in seen:
-                return True
-            seen.add(num)
-        return False
-           
         
+
+
+
+
+
+
+
+        numss=set()
+
+        for i in nums:
+            if i in numss:
+                return True
+            numss.add(i)
+
+        return False
